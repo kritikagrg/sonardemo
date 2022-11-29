ded5b364b07534a4a2bbb44b1c380596f105783e
kritikagrg <kritika@screenengineering.com>
2022-11-29 19:46:44 +0530

    chck

--- app/manage.py
+++ app/manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Django's command-line tility for administrative tasks."""
 import os
 import sys
 
@@ -13,7 +13,7 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "forget to activate a virtual environent?"
         ) from exc
     execute_from_command_line(sys.argv)
 
